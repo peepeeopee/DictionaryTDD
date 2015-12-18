6417f4dbb942f78b965c9a6e10d2df63564e751c
Peter Antoncich <antoncichp@gmail.com>
2015-12-18 14:35:29 +0200

    set up boiler plate

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    /*
+     * Create a method that takes a string and a dictionary, and replaces every key in the dictionary pre and suffixed with a dollar sign, with the corresponding value from the Dictionary.
+
+Tests
+
+input : "", dict empty, output:""
+
+input : "$temp$", dict ["temp", "temporary"], output: "temporary"
+
+input : "$temp$ here comes the name $name$", dict ["temp", "temporary"] ["name", "John Doe"], output : "temporary here comes the name John Doe"
+     */
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+        
+    }
+}
